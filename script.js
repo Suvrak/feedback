ddd0a6da4ab53ec7cdfaf123bfdd21e379a2d8d3
Suvrak <Sualva07@mail.ru>
2025-01-14 21:03:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-document.querySelectorAll('.folder, .file').forEach(item => {
+document.querySelectorAll('.folder').forEach(item => {
     item.addEventListener('click', function (e) {
         e.stopPropagation(); // предотвращаем всплытие события
         const info = document.getElementById('info');
-        
-        // Проверяем, является ли элемент папкой или файлом
-        if (this.classList.contains('folder')) {
-            info.textContent = Вы выбрали папку: ${this.childNodes[0].nodeValue.trim()};
-        } else if (this.classList.contains('file')) {
-            info.textContent = Вы выбрали файл: ${this.childNodes[0].nodeValue.trim()};
-        }
+        const toggle = this.querySelector('.toggle');
 
-        this.classList.toggle('open'); // Открываем/закрываем папку при клике
+        // Открываем/закрываем папку
+        this.classList.toggle('open');
+        if (this.classList.contains('open')) {
+            toggle.textContent = '-'; // Меняем на минусик
+        } else {
+            toggle.textContent = '+'; // Меняем на плюсику
+        }
     });
 });
+
+document.querySelectorAll('.device').forEach(item => {
+    item.addEventListener('click', function (e) {
+        e.stopPropagation(); // предотвращаем всплытие события
+        const info = document.getElementById('info');
+        info.textContent = Вы выбрали устройство: ${this.textContent};
+    });
+});
+
