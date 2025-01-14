20adf65ac6b6091f57b224b07ffcff53b245fdd7
Suvrak <Sualva07@mail.ru>
2025-01-14 20:30:37 +0300

    Update and rename FileItem.cs to none

--- FileItem.cs
+++
@@ -1,9 +0,0 @@
-namespace YourNamespace.Models
-{
-    public class FileItem
-    {
-        public string Name { get; set; }
-        public bool IsFolder { get; set; }
-        public List<FileItem> Children { get; set; } = new List<FileItem>();
-    }
-}
